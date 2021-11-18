6043b3e370643b4607ec11469dce0ff7d8307249
Aadhinana <ajibaa@gmail.com>
2021-11-18 21:09:04 +0530

    WIP: Add failing test, fix IDL first

--- tests/misc/programs/shared/src/lib.rs
+++ tests/misc/programs/shared/src/lib.rs
@@ -1,7 +1,74 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+mod shared {
+    use super::*;
+
+    // Normal
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
     }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+
+    //  Vectors
+    // pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
+    //     let my_account = &mut ctx.accounts.my_account;
+    //     for acc in my_account.into_iter(){
+    //         acc.data = data;
+    //     }
+    //     // my_account.data = data;
+    //     Ok(())
+    // }
+    // pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
+    //     let my_account = &mut ctx.accounts.my_account;
+    //     for acc in my_account.into_iter(){
+    //         acc.data = data;
+    //     }
+    //     // my_account.data = data;
+    //     Ok(())
+    // }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+// Vectors
+// #[derive(Accounts)]
+// pub struct Initialize<'info> {
+//     #[account(init, payer = user, space = 8 + 8)]
+//     pub my_account: Vec<Account<'info, MyAccount>>,
+//     #[account(mut)]
+//     pub user: Signer<'info>,
+//     pub system_program: Program<'info, System>,
+// }
+// #[derive(Accounts)]
+// pub struct Update<'info> {
+//     #[account(mut)]
+//     pub my_account: Vec<Account<'info, MyAccount>>,
+// }
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
 }
